51325ea88cb925406935819bab449ccec6688f1a
feikardev <cantelligiovana@gmail.com>
2025-06-02 10:47:03 -0300

    Commit mini

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ npm install tailwindcss @tailwindcss/vite lucide-react
 Estrutura:
 Na pasta components há Repositorio.jsx, que contém a lógica até então implementada:
 
-As constantes são para manejo de estados, e atualmente há um array para renderizar no máximo 10 vezes um card com scroll de repositório caso o form de pesquisa seja enviado por meio de um click no botão. Também há um protótipo de favorito, mudando por estado de click.
+As constantes são para manejo de estados, e atualmente há um array para renderizar no máximo 10 vezes um card com scroll de repositório caso o form de pesquisa seja enviado por meio de um click no botão. Também há um protótipo de favorito, mudando o ícone por estado de click, além de tratamentos de erro.
 
 Esta é a primeira semana que aprendo React.
--- src/components/Repositorio.jsx
+++ src/components/Repositorio.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
-import { Heart, HeartOff } from 'lucide-react';
-
+import { Loader, Heart, HeartOff } from 'lucide-react'
 
 export default function Repositorio() {
   const [clicou, setClicou] = useState(false)
   const [pesquisou, setPesquisou] = useState(null)
+  const [carregando, setCarregando] = useState(false)
   const [repositorios, setRepositorios] = useState([])
+  const [erro, setErro] = useState(null)
   const array10 = repositorios.slice(0, 10)
 
   async function handlePesquisar(formData) {
@@ -22,11 +23,27 @@ export default function Repositorio() {
   }
 
   async function fetchRepositorios(usuario) {
+    setCarregando(true)
+    setErro(null)
     const res = await fetch(`https://api.github.com/users/${usuario}/repos`)
     const data = await res.json()
     setRepositorios(data)
+    if (repositorios != []) {
+      setCarregando(false)
+      // console.log(carregando)
+    }
+    if (res.status === 404) {
+      setCarregando(false)
+      setErro(`Usuário "${usuario}" não encontrado.`)
+      setRepositorios([])
+    } else if (res.status === 403) {
+      setErro("Limite de pesquisas excedido.")
+    }
+    if (data.length === 0) {
+      setErro("Nenhum repositório.")
+    }
   }
-  
+
   return (
     <>
       <header className="header1"><b>GitHub, mas simples</b></header>
@@ -40,25 +57,27 @@ export default function Repositorio() {
         </input>
         <button className="button1">Pesquisar</button>
       </form> 
+
+      {carregando ? <Loader  className="loader"/> : null}
       {pesquisou ?
-      (
-      <>
-        <h1 className="h1-repositorio">Repositórios de {pesquisou}</h1>
-        {array10.map((repositorio) => (
-          <div className="div-repositorio" key={repositorio.id}>
-            <ul className="list-usuario">
-              <li>{repositorio.name}</li>
-              {repositorio.description ? <li>{repositorio.description}</li> : null}
-              <li>Avaliação: {repositorio.stargazers_count}</li>
-              <li><a href={repositorio.html_url}>Link</a></li>
-              <li onClick={() =>handleClicou(repositorio.id)}>
-                {clicou[repositorio.id] ? (<HeartOff />) : (<Heart />)}
-              </li>
-            </ul>
-          </div>
-        ))}
-      </>
-      ) : null}
+        (
+          <>
+            {erro ? <p className="p-erro">{erro}</p> : <h1 className="h1-repositorio">Repositórios de {pesquisou}</h1>}
+            {array10.map((repositorio) => (
+              <div className="div-repositorio" key={repositorio.id}>
+                <ul className="list-usuario">
+                  <li>{repositorio.name}</li>
+                  {repositorio.description ? <li>{repositorio.description}</li> : null}
+                  <li>Avaliação: {repositorio.stargazers_count}</li>
+                  <li><a href={repositorio.html_url}>Link</a></li>
+                  <li onClick={() =>handleClicou(repositorio.id)}>
+                    {clicou[repositorio.id] ? (<HeartOff />) : (<Heart />)}
+                  </li>
+                </ul>
+              </div>
+          ))}
+          </>
+        ) : null}
     </>
   )
 }
